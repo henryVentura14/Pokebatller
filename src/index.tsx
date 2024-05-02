89191a4f30a87dadb75c95da84aadaa7ef2c6788
Henry J Ventura G <henry.ventura14@outlook.es>
2024-05-01 21:38:25 -0500

    Update
    update pokeCard component,
    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import { PokeProvider } from './context/PokeContext';
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <PokeProvider>
       <App />
     </PokeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
